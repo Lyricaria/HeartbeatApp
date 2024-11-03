ae7c7a5a9ea97411e2ac82427ae66e7c52ad1453
Lyricaria <lyricaria.game@gmail.com>
2024-11-03 13:31:34 +0000

    Refactor Program.cs

--- HeartbeatApp/Program.cs
+++ HeartbeatApp/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// NATS URL configuration
+using HeartbeatApp;
+using HeartbeatApp.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+var natsUrl = "nats://localhost:4222";
+
+// Create the HostBuilder
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        // Register HeartbeatEvent as a singleton service
+        services.AddSingleton(new HeartbeatEvent(natsUrl));
+
+        // Add Quartz services
+        services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+
+            // Register the job and trigger
+            var jobKey = new JobKey("HeartbeatJob");
+
+            q.AddJob<HeartbeatJob>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("HeartbeatJob-trigger")
+                .WithCronSchedule("0/15 * * * * ?")); // Every 15 seconds
+        });
+
+        // Add Quartz.NET hosted service
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+    });
+
+// Build and start the host
+using var host = builder.Build();
+await host.StartAsync();
+
+// Start the HeartbeatEvent connection
+var heartbeatEvent = host.Services.GetRequiredService<HeartbeatEvent>();
+await heartbeatEvent.StartAsync();
+
+Console.WriteLine("HeartbeatApp is running. Press Ctrl+C to shut down.");
+
+// Wait for the application to exit
+await host.WaitForShutdownAsync();
+
+// Dispose of the HeartbeatEvent when the application is shutting down
+await heartbeatEvent.DisposeAsync();

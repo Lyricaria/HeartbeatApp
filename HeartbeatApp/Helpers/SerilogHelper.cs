d13b401f833d7592dc5d12fa027f09431f3521f0
Satho-M <rui92rodrigues@gmail.com>
2024-11-03 17:24:48 +0000

    Add Helper classes for Program.cs organization

---
+++ HeartbeatApp/Helpers/SerilogHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace HeartbeatApp.Helpers
+{
+    public static class SerilogHelper
+    {
+        public static ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: "logs/heartbeatapp.log",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: LogEventLevel.Information)
+                .CreateLogger();
+        }
+    }
+}
---
+++ HeartbeatApp/Helpers/ServiceConfigurator.cs
@@ -0,0 +1,35 @@
+﻿using HeartbeatApp.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+namespace HeartbeatApp.Helpers
+{
+    public static class ServiceConfigurator
+    {
+        public static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+        {
+            var configuration = hostContext.Configuration;
+            var natsUrl = configuration.GetSection("Nats")["Url"];
+
+            services.AddSingleton(new HeartbeatEvent(natsUrl));
+
+            //Quartz
+            services.AddQuartz(q =>
+            {
+                q.UseMicrosoftDependencyInjectionJobFactory();
+
+                var jobKey = new JobKey("HeartbeatJob");
+
+                q.AddJob<HeartbeatJob>(opts => opts.WithIdentity(jobKey));
+
+                q.AddTrigger(opts => opts
+                    .ForJob(jobKey)
+                    .WithIdentity("HeartbeatJob-trigger")
+                    .WithCronSchedule("0/15 * * * * ?")); // Every 15 seconds
+            });
+
+            services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+        }
+    }
+}

2065f5f7526934601880b507e164e8be145954e7
Lyricaria <lyricaria.game@gmail.com>
2024-11-03 13:31:00 +0000

    Add HearbeatJob

---
+++ HeartbeatApp/Jobs/HeartbeatJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+
+namespace HeartbeatApp.Jobs
+{
+    public class HeartbeatJob : IJob
+    {
+        private readonly HeartbeatEvent _heartbeatEvent;
+
+        public HeartbeatJob(HeartbeatEvent heartbeatEvent)
+        {
+            _heartbeatEvent = heartbeatEvent;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await _heartbeatEvent.InvokeAsync();
+        }
+    }
+}
